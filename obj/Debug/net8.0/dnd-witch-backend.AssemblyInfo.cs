b154cb727ae57597f7a5b062049ce11990824405
asseternity <nakupovasset@gmail.com>
2025-10-09 17:26:32 +0400

    feat: build project

--- obj/Debug/net8.0/dnd-witch-backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/dnd-witch-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dnd-witch-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ce28744a1b36724a85db709f843709b5673a5db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce8fa177922b3da23bac38909fac7228afe26d41")]
 [assembly: System.Reflection.AssemblyProductAttribute("dnd-witch-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dnd-witch-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
